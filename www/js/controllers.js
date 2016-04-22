ff4407d0f9ebeb74f1c2a1c7d6cd8ef1df64c468
gerardnaughton7 <gerardnaughton7@gmail.com>
2016-04-22 17:24:07 +0100

    4th commit start of structure

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('AddTaskCtrl', function($scope) {})
-
-.controller('WorkCtrl', function($scope) {})
-
-.controller('PersonalCtrl', function($scope) {
-  var data = {
+.controller('AddTaskCtrl', function($scope, Task) {
+    var data = {
     newTask: ""
   };
   
@@ -16,5 +12,10 @@ angular.module('starter.controllers', [])
   
   $scope.data = data;
   $scope.addTask = addTask;
+})
 
+.controller('WorkCtrl', function($scope) {})
+
+.controller('PersonalCtrl', function($scope,Task) {
+  $scope.data = Task.data;
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,20 @@
 angular.module('starter.services', [])
 
-.factory('Work', function() {
+.factory('Task', function() {
   
+  var data = {
+    tasks: [
+      
+    ]
+  };
+  
+  function addTask(title) {
+    data.tasks.push({title: title, added: new Date()});
+  }
+  
+  return {
+    data: data
+    , addTask: addTask
+  };
 
 });
